93710b908d52d4abd0f25da982003fdfd97b1e83
Thomas <hallohein@web.de>
2023-08-16 17:43:16 +0200

    First Navbar

---
+++ components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+
+<template>
+
+<div>
+    <h1>Mega Test Hello World</h1>
+    <v-text-field />
+    <v-btn variant="elevated">
+        Knopf
+    </v-btn>
+</div>
+
+</template>
